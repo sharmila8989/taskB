36fe50999874c79cc2759fdf33d2f27ee31d14f5
sharmila8989 <sharmila8989@users.noreply.github.com>
2018-08-03 16:05:28 +1000

    Failing Unit Test

---
+++ WebApplicationTaskB/WebApplicationTaskB.Tests/Controllers/ExampleControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+using WebApplicationTaskB;
+using WebApplicationTaskB.Controllers;
+
+
+namespace WebApplicationTaskB.Tests.Controllers
+{
+    [TestClass]
+    public class ExampleControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            ExampleController controller = new ExampleController();
+
+            // Act
+            IEnumerable<string> result = controller.Get();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Count());
+            Assert.AreEqual("Wrong", result.ElementAt(0));
+            Assert.AreEqual("value2", result.ElementAt(1));
+        }
+    }
+}
